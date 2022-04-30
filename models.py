ef839bbd01f87b28c45e9cb52b7fd9e11bc42a5e
Aaris-Kazi <aariskazi@gmail.com>
2022-04-30 08:32:45 +0530

    Models with required tables

---
+++ models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+
+from database import Base
+
+class Students(Base):
+    __tablename__ = "Students"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    # items = relationship("Item", back_populates="owner")
+
+class Books(Base):
+    __tablename__ = "Books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    available = Column(Boolean, default=True)
+    
+class Management(Base):
+    __tablename__ = "Management"
+
+    id = Column(Integer, primary_key=True, index=True)
+    student_name = Column(String, unique=True, index=True)
+    Book_name = Column(String, index=True)
+    returned = Column(Boolean, default=True)
+    
